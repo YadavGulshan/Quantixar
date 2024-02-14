ba01f3aebc4e4ec8d1920b0d667b59023e9f7be9
rahul007-bit <yadavrahul8141@yahoo.com>
2024-02-14 17:00:48 +0530

    fix swagger

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,55 +1,55 @@
+pub mod errors;
+pub mod hanlders;
+pub mod routes;
+
 use std::io::Error;
 use std::net::{Ipv4Addr, SocketAddr};
 
 use axum::Router;
+use routes::dataset;
 use tokio::{net::TcpListener, signal};
 use tower_http::cors::{Any, CorsLayer};
 use utoipa::OpenApi;
 use utoipa_rapidoc::RapiDoc;
 use utoipa_redoc::{Redoc, Servable};
 use utoipa_swagger_ui::SwaggerUi;
-
-pub mod errors;
-pub mod routes;
-pub mod hanlders;
-
 #[derive(OpenApi)]
-#[openapi()]
+#[openapi(paths(dataset::list_datasets), components(schemas(dataset::DataSet)))]
 struct ApiDoc;
 
 pub async fn init() -> Result<(), Error> {
-  let cors = CorsLayer::default().allow_origin(Any).allow_headers(Any).allow_methods(Any);
-  let dataset_api = routes::dataset::data_set_router();
-  let app = Router::new()
-          .layer(cors)
-          .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-          .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
-          .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
-          .nest("/", dataset_api);
+    let cors = CorsLayer::default().allow_origin(Any).allow_headers(Any).allow_methods(Any);
+    let dataset_api = routes::dataset::data_set_router();
+    let app = Router::new()
+        .layer(cors)
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
+        .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
+        .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
+        .nest("/", dataset_api);
 
-  let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8080));
-  let listener = TcpListener::bind(&address).await?;
-  log::info!("Listening on {}", address);
-  axum::serve(listener, app.into_make_service()).await
+    let address = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 8945));
+    let listener = TcpListener::bind(&address).await?;
+    log::info!("Listening on {}", address);
+    axum::serve(listener, app.into_make_service()).await
 }
 
 async fn shutdown_signal() {
-  let ctrl_c = async {
-    signal::ctrl_c().await.expect("failed to install Ctrl+C handler");
-  };
+    let ctrl_c = async {
+        signal::ctrl_c().await.expect("failed to install Ctrl+C handler");
+    };
 
-  #[cfg(unix)]
-          let terminate = async {
-    signal::unix::signal(signal::unix::SignalKind::terminate())
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
             .expect("failed to install signal handler")
             .recv()
             .await;
-  };
+    };
 
-  #[cfg(not(unix))]
-          let terminate = std::future::pending::<()>();
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
 
-  tokio::select! {
+    tokio::select! {
         _ = ctrl_c => {},
         _ = terminate => {},
     }
